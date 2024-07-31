39fef0de6e66e5ab8093e287bfbc1f4ebbe5ea16
Apurv Arya <apurvarya19@gmail.com>
2024-08-01 02:58:20 +0530

    Update

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "28611965"))
 API_HASH = getenv("API_HASH", "d36cfa9250dd3d0d46678b538836ca8b")
-BOT_TOKEN = getenv("BOT_TOKEN", "7270342784:AAHEzCyWZ4xyBpja1Z9ABBOhlKcCXL02jgs")
+BOT_TOKEN = getenv("BOT_TOKEN", "7458904383:AAFzZ32HCJtog-L6H3UPn7B4cTupiJnv9PI")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1002223221910"))

85acb0d789054f02fc0ffc7fc8b8489d250c850a
Apurv Arya <apurvarya19@gmail.com>
2024-07-31 17:31:01 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# devggn
+
+# Note if you are trying to deploy on vps then directly fill values in ("")
+
+from os import getenv
+
+API_ID = int(getenv("API_ID", "28611965"))
+API_HASH = getenv("API_HASH", "d36cfa9250dd3d0d46678b538836ca8b")
+BOT_TOKEN = getenv("BOT_TOKEN", "7270342784:AAHEzCyWZ4xyBpja1Z9ABBOhlKcCXL02jgs")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002223221910"))
